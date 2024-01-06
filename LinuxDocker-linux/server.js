6d5536b57341c719fff4be175a960b84cc3b77ce
sayalipatil13 <144783494+sayalipatil13@users.noreply.github.com>
2024-01-06 09:37:06 +0530

    Update server.js

--- LinuxDocker-linux/server.js
+++ LinuxDocker-linux/server.js
@@ -12,3 +12,20 @@ app.get("/product", (req, res) =>
 });
 app.listen(8000);
 console.log("Listening on port 8000");
+
+
+1. git add .
+2. git status
+3. git commit -m "added"
+4. git push
+
+
+1. sudo apt update
+2. ls
+3. sudo apt install
+4. sudo apt install docker.io
+5. sudo docker build -t appname
+sudo docker appname
+sudo docker run -d -p 8000:8000 appname 
+9. copy 8000 on browser
+
